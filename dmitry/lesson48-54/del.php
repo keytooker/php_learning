cb6410afead28cc713d56f372ad331722a93691a
M.Visloguzov <keytooker@gmail.com>
2019-07-26 21:32:50 +0300

    Урок 54 задача 7 решена.

--- dmitry/lesson48-54/del.php
+++ dmitry/lesson48-54/del.php
@@ -1,17 +1,22 @@
 <?php
 /**
- * changepassword.php
+ * del.php
  *
  * Урок 54
- * Задача 5
- * Реализовать смену пароля
+ * Задача 7
+ * Реализуйте удаление аккаунта.
  */
 
 session_start();
-
+?>
+<p>
+Вы вошли как <?= $_SESSION['login']; ?>
+</p>
+<?php
 //  в начале обязательно следует сделать проверку на то, авторизован ли пользователь вообще.
 if ( isset($_SESSION['auth']) and ($_SESSION['auth'] == true) and (!empty($_POST)) ) {
     $id = $_SESSION['id']; // id юзера из сессии
+
     $host = 'localhost'; //имя хоста, на локальном компьютере это localhost
     $user = 'root'; //имя пользователя, по умолчанию это root
     $password = ''; //пароль, по умолчанию пустой
@@ -28,19 +33,14 @@ if ( isset($_SESSION['auth']) and ($_SESSION['auth'] == true) and (!empty($_POST
     $hash = $user['password']; // соленый пароль из БД
 
 // Проверяем соответствие хеша из базы введенному старому паролю
-    if (password_verify($_POST['old_password'], $hash)) {
-        // Все ок, меняем пароль...
+    if (password_verify($_POST['password'], $hash)) {
 
-        // Хеш нового пароля:
-        $newPasswordHash = password_hash($_POST['new_password'], PASSWORD_DEFAULT);
-
-        if ($_POST['new_password'] == $_POST['confirm']) {
-            // Выполним UPDATE запрос:
-            $query = 'UPDATE users SET password=\'' . $newPasswordHash . '\' WHERE id=' . $id;
+        if ($_POST['password'] == $_POST['confirm']) {
+            $query = 'DELETE FROM users WHERE id=' . $id;
 
             mysqli_query($link, $query);
 
-            echo 'Пароль сменен.';
+            echo 'Пользоатель удален';
         }
         else
         {
@@ -48,17 +48,14 @@ if ( isset($_SESSION['auth']) and ($_SESSION['auth'] == true) and (!empty($_POST
         }
     } else {
         // Старый пароль введен неверно, выведем сообщение
-        echo 'Старый пароль введен неверно!';
+        echo 'Неверный пароль';
     }
 }
 
 ?>
 
 <form method="POST">
-    <p>login: <label><input name="login"/></label></p>
-
-    <p>старый: <label><input type="password" name="old_password"/></label></p>
-    <p>новый: <label><input type="password" name="new_password"/></label></p>
+    <p>пароль: <label><input type="password" name="password"/></label></p>
     <p>подтвердить: <label><input name="confirm" type="password"></label></p>
-    <input type="submit" value="Отправить"/>
+    <input type="submit" value="Удалить"/>
 </form>
